05de2223586ba0d42b62ef566b78d4cda3edd13b
Jill-Jênn Vie <vie@jill-jenn.net>
2019-11-20 21:28:45 +0100

    Add a link counter per domain

--- problems/tex2problems.py
+++ problems/tex2problems.py
@@ -11,10 +11,12 @@ l'option -yaml n'est plus d'utilité
 
 from sys import argv
 import re
+from collections import Counter
+from urllib.parse import urlsplit
 
 
 def make_pb(chapter, name, order, links):
-	return
+    return
 
 CHAPTER = "%chapter="
 ORDER= "%difficulty="
@@ -25,54 +27,62 @@ regexp = re.compile(r"\\href\{([^\}]*)\}\{([^\}]*)\}")
 mode = argv[1][1:]
 
 if mode == "show":
-	print('<table class="sortable"><tr><th>chapter</th><th>difficulty</th><th>problem</th><th>judge</th></tr>')
+        print('<table class="sortable"><tr><th>chapter</th><th>difficulty</th><th>problem</th><th>judge</th></tr>')
 elif mode == "hide":
-	print('<table class="sortable"><tr><th>difficulty</th><th>problem</th><th>judge</th></tr>')
+    print('<table class="sortable"><tr><th>difficulty</th><th>problem</th><th>judge</th></tr>')
 else:
-	print("---")
+    print("---")
 
 
 def print_problem(chapter, name, order, links):
-	judge = ", ".join('<a href="%s">%s</a>' % code for code in links)
-	if order is not None:
-		diff = "&star;" * order
-	else:
-		diff = ""
-	if mode == "yaml":
-		print("- chapter: {}\n  name:  {}".format(chapter, name))
-		if order is not None:
-			print("  order:  ", order)
-		print("  links:")
-		for url, code in links:
-			print("    - url:", url)
-			print("      code:", code)
-		print()
-	elif mode == "show":
-		print('<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(chapter, diff, name, judge))
-	else:	
-		print('<tr><td>{}</td><td>{}</td><td>{}</td></tr>'.format(diff, name, judge))
+    judge = ", ".join('<a href="%s">%s</a>' % code for code in links)
+    if order is not None:
+        diff = "&star;" * order
+    else:
+        diff = ""
+    if mode == "yaml":
+        print("- chapter: {}\n  name:  {}".format(chapter, name))
+        if order is not None:
+            print("  order:  ", order)
+        print("  links:")
+        for url, code in links:
+            print("    - url:", url)
+            print("      code:", code)
+        print()
+    elif mode == "show":
+        print('<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(chapter, diff, name, judge))
+    else:   
+        print('<tr><td>{}</td><td>{}</td><td>{}</td></tr>'.format(diff, name, judge))
 
 
+c = Counter()
 for filename in argv[2:]:
-	f = open(filename, "r")
-	chapter = filename.split('/')[-1].split('.')[0]
-	for line in f:
-		if line.startswith("%chapter:"):
-			chapter = line()
-		if "\\href" in line:
-			i = line.find("\\href")
-			name = line[:i].strip()
-			links = []
-			for link in regexp.finditer(line):
-				links.append( (link.group(1), link.group(2)) )
-			if ORDER in line:
-				i = line.find(ORDER) + len(ORDER)
-				diff = int(line[i:].split()[0])
-				print_problem(chapter, name, diff, links)
-			else:
-				print_problem(chapter, name, None, links)
+    f = open(filename, "r")
+    chapter = filename.split('/')[-1].split('.')[0]
+    for line in f:
+        if line.startswith("%chapter:"):
+            chapter = line()
+        if "\\href" in line:
+            i = line.find("\\href")
+            name = line[:i].strip()
+            links = []
+            for link in regexp.finditer(line):
+                url = link.group(1)
+                domain = urlsplit(url).netloc
+                c[domain] += 1
+                links.append( (url, link.group(2)) )
+            if ORDER in line:
+                i = line.find(ORDER) + len(ORDER)
+                diff = int(line[i:].split()[0])
+                print_problem(chapter, name, diff, links)
+            else:
+                print_problem(chapter, name, None, links)
 
 
 
 if mode != "yaml":
-	print('</table>')
+    print('</table>')
+
+
+for k, v in c.most_common():
+    print(k, v)
