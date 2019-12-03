870038b6db0c2c894d1533fff5de157a76dc593b
Christoph Dürr <xtof.durr@gmail.com>
2019-12-03 11:13:01 +0100

    new problem list

--- problems/grep2problems.py
+++ problems/grep2problems.py
@@ -3,13 +3,13 @@
 """
 usage:
 
-./tex2problems.py -show ~/Documents/CUP-vie-durr/*/*.tex > ../_includes/problems_show.html 
-./tex2problems.py -hide ~/Documents/CUP-vie-durr/*/*.tex > ../_includes/problems_hide.html 
+grep href  ~/Documents/CUP-vie-durr/*/*.tex | ./grep2problems.py > ../_includes/problems_show.html 
+similar for -hide
 
 l'option -yaml n'est plus d'utilité
 """
 
-from sys import argv, stderr
+from sys import argv, stderr, stdin
 import re
 from collections import Counter
 from urllib.parse import urlsplit
@@ -28,6 +28,8 @@ ORDER= "%difficulty="
 regexp = re.compile(r"\\href\{([^\}]*)\}\{([^\}]*)\}")
 
 mode = argv[1][1:]
+
+test_urls = len(argv) == 3 
 
 if mode == "show":
         # print('<table class="sortable"><tr><th>chapter</th><th>difficulty</th><th>problem</th><th>judge</th></tr>')
@@ -64,46 +66,37 @@ def print_problem(chapter, name, order, links):
         print('<tr><td>{}</td><td>{}</td></tr>'.format(name, judge))
 
 
-c = Counter()
-for filename in argv[2:]:
-    f = open(filename, "r")
-    chapter = filename.split('/')[-1].split('.')[0]
-    for line in f:
-        if line.startswith("%chapter:"):
-            chapter = line()
-        if "\\href" in line: # and line[0] != '%': # ignore references commented out 
-            print(line)
-            i = line.find("\\href")
-            name = line[:i].strip()
-            links = []
-            for link in regexp.finditer(line):
-                url = link.group(1)
-                domain = urlsplit(url).netloc
-                c[domain] += 1
-                links.append( (url, link.group(2)) )
-                print(name, "\t", url, file=stderr)
+# pattern = re.compile("([^\\]*)\\.*:([^\\]*)\\\\href\{([^\}]*)\}\{([^\}]*)\}.*")
+pattern = re.compile(r"([^\\]*)\\[^:]*:([^\\]*)\\href{([^}]*)}{([^}]*)}")
+for line in stdin:
+    tab = pattern.match(line)
+    if tab is not None:
+        # print(tab[1], tab[2], tab[3], tab[4])
+        chapter = tab[1]
+        name = tab[2]
+        links = [(tab[3], tab[4])]
+        print_problem(chapter, name, None, links)
+        if tab[2][0] == "%":
+            # ignore commented out problems
+            continue
+        if test_urls:
+            for url, code in links:
                 dt = time.time()
                 try:
                     r = urlopen(url)
-                    print('=>', r.getcode(), '{:.3f}s'.format(time.time() - dt), file=stderr)
+                    if r.getcode() != 200:
+                        print('=>', r.getcode(), '{:.3f}s'.format(time.time() - dt), url, code, file=stderr)
                 except urllib.error.HTTPError:
-                    print('=> 404', file=stderr)
+                    print('=> 404', url, code, file=stderr)
                 except urllib.error.URLError as e:
                     print('=> error', e, file=stderr)
                 except ConnectionResetError:
                     print('=> server hung up', file=stderr)
-            if ORDER in line:
-                i = line.find(ORDER) + len(ORDER)
-                diff = int(line[i:].split()[0])
-                print_problem(chapter, name, diff, links)
-            else:
-                print_problem(chapter, name, None, links)
+    else:
+        print("does not match", line, file=stderr)
 
 
 
 if mode != "yaml":
     print('</table>')
 
-
-for k, v in c.most_common():
-    print(k, v)
